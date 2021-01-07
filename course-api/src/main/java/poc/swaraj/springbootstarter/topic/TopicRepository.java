ba7612907599f4621981dffd20e9881130a06c27
Swaraj Shandilya <swarajshandilya@users.noreply.github.com>
2021-01-07 12:02:02 -0500

    added  course api

---
+++ course-api/src/main/java/poc/swaraj/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package poc.swaraj.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String>{
+
+}
